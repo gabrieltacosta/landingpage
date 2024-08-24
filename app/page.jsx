2a94be5368666dd74e28880eccdf2ae40b61624a
gabrieltacosta <gabriel.tuka@gmail.com>
2024-08-24 15:48:07 -0300

    Criando sessão Welcome

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header/page";
+import Welcome from "@/components/welcome/page";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
       <div className={styles.container}>
         <Header />
+        <Welcome />
       </div>
   );
 }
