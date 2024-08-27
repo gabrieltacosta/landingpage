d37aa19ca42c3236a176e20ac3e095aba5c81ef8
Gabriel Costa <107140329+gabrieltacosta@users.noreply.github.com>
2024-08-26 22:57:16 -0300

    Update page.jsx

--- components/header/page.jsx
+++ components/header/page.jsx
@@ -15,7 +15,6 @@ const Header = () => {
                 <Link href="#">Home</Link>
                 <Link href="#">O que fazemos</Link>
                 <Link href="#">Cases</Link>
-                <Link href={"#"}>Tarefas</Link>
             </div>
             <div className={Styles.action}>
                 <Button title="Fale conosco" />
